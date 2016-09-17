d506fe6e133eba125553df9f0f4600ff9e0f5955
Dave Belcher <ukbelch@gmail.com>
2016-09-17 11:13:22 -0700

    Adding config file

---
+++ example_config.py
@@ -0,0 +1,12 @@
+# to use, remove the example_ from the filename and provide
+# all of the values in <>.
+
+# Database config
+dbhost = "<db host>"
+dbuser = "<db user>"
+dbpass = "<db pass>"
+dbname = "<barcode_service>"
+
+# Outpan API
+api_url = "<api URL>"
+api_key = "<api key>"
